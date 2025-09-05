ce62f970c339b559ded38328d9037ce0f6c0cfc8
Aneesh <aneeshy1508cse.com>
2025-09-06 00:39:28 +0530

    First commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { serve } from "inngest/express";
+import {inngest, functions} from './inngest/inde.js'
 
 const app = express();
 
@@ -11,7 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res)=> res.send('server is runnoing'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 4000;
+
 
 app.listen(PORT, ()=> console.log(`Server is runnning on port ${PORT}`))
