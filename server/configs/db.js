4bdc46fae1101fc3012f421c3e7ab88ae34258c4
Aneesh <aneeshy1508cse.com>
2025-09-21 21:02:43 +0530

    add backend

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,13 +1,27 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         mongoose.connection.on('connected', () => console.log('Database Connected'))
+//         await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
+        
+//     } catch (error) {
+//         console.log(error.message)
+        
+//     }
+// }
+// export default connectDB
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on('connected', () => console.log('Database Connected'))
-        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
-        
-    } catch (error) {
-        console.log(error.message)
-        
-    }
-}
-export default connectDB
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database Connected");
+  } catch (error) {
+    console.error("DB Connection Error:", error.message);
+    throw error; // important to let the server know
+  }
+};
+
+export default connectDB;
