a6f867848e33608cbc8bf34133963bf185513c45
Aneesh <aneeshy1508cse.com>
2025-09-22 10:48:59 +0530

    changes

--- server/api/index.js
+++
@@ -1,4 +0,0 @@
-import app from "../server.js"; // your express app
-import serverless from "serverless-http";
-
-export default serverless(app);
--- server/api/inngest.js
+++
@@ -1,7 +0,0 @@
-import { serve } from "inngest/vercel"; // or "inngest/vercel" if not Next.js
-import { inngest, functions } from "../inngest/index.js"; 
-
-export default serve({
-  client: inngest,
-  functions,
-});
